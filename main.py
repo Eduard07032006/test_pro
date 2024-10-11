ce3924282618b94c236cd8b642eaec4382bd0c21
kazim704 <salavatovasakinat25@gmail.com>
2024-10-11 16:31:34 +0500

    Eduard1

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print(f'Hi, My first git program')
+
+
+if __name__ == '__main__':
+    main()
+
+def print_hi():
+    print(f'Hi, My first git program')
+    print(f'Hello, Git!')
+if __name__ == '__main__':
+    print_hi('PyCharm')
